a47f54ea21e969f536ea00437c5a028ba54ccda0
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-09-15 15:16:36 +0300

    feat :userpage:

--- FrontEnd/inventory-manager/components/Header.js
+++ FrontEnd/inventory-manager/components/Header.js
@@ -1,19 +1,18 @@
 export default function Header(props) {
-    // funtion that will update the count
+    const logout = () => {
+        localStorage.removeItem("accessToken")
+        window.location.href = '/Login'
+    }
     return (
-        <header className="flex justify-between p-4 bg-green-600 text-gray-50">
+        <header className="flex justify-between p-4 bg-green-500 text-gray-50">
             <h1 className="text-3xl font-bold">
                 Inventer
             </h1>
             <div>
-                <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <p>New Invoice</p>
-                </button>
-                <button type="submit" class="px-4 py-2.5 ml-2 text-sm font-medium text-white bg-gray-400 rounded-full border border-gray-400 hover:bg-gray-600 ">
-                    <p>Admin</p>
+                <button onClick={logout} type="submit" className="border-2 border-white rounded-full px-8 py-2 inline-block font-semibold bg-green-500 text-white hover:bg-white hover:border-green-500 hover:text-green-500 ">
+                    <p>Logout</p>
                 </button>
             </div>
-             
         </header>
     )
 }
--- FrontEnd/inventory-manager/components/search.js
+++ FrontEnd/inventory-manager/components/search.js
@@ -1,19 +1,20 @@
-export default function Search(props){
+import { useState } from "react"
+
+export default function Search(props) {
+    
     return (
         <div className='flex justify-between mt-5'>
-       <p> Users Page</p>
-        <form class="flex items-center ">   
-            <label for="simple-search" class="sr-only">Search</label>
-            <div class="relative ">
-                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <p> Users Page</p>
+            <form class="flex items-center mr-20 mt-10">
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative ">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <input  type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-48 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
                 </div>
-                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-48 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
-            </div>
-            <button onClick={props.func} type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <p> {props.name} </p>
-               </button>
-        </form>
-        </div> 
+                
+            </form>
+        </div>
     )
 }
