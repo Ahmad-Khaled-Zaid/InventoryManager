06cbc78d4f847b8a4750c905a50e6a8e59927977
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-09-15 19:03:14 +0300

    feat: user page

--- FrontEnd/inventory-manager/components/Modal.js
+++ FrontEnd/inventory-manager/components/Modal.js
@@ -4,7 +4,7 @@ const Modal = ({ isVisible ,onClose,children}) => {
         if(e.target.id==='wrapper') onClose();
     }
     return (
-        <div id="wrapper" className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" onClick={handleClose}>
+        <div id="wrapper" className=" modal fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" onClick={handleClose}>
             <div className="w-[600px] flex flex-col">
                 <button className="text-white text-xl place-self-end" onClick={()=>onClose()}>X</button>
                 <div className="bg-white p-2 rounded">
